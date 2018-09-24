c80e117dcbfd25fe4aaffdde2af5f6aeaca9a766
dkgian <duykhanhgian0510@gmail.com>
2018-09-24 23:54:09 +0200

    update key

--- src/action/index.js
+++ src/action/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = ''
+const API_KEY = process.env.API_KEY
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
@@ -8,7 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather (city) {
   const url = `${ROOT_URL}&q=${city}`
   const request = axios.get(url)
-  console.log(typeof request)
+
   return {
     type: FETCH_WEATHER,
     payload: request,
